f4ae931de8f2ad694370e338bc2fe2e0858a2eb8
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-08-15 23:06:01 +0200

    all about route

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+//php artisan route:list  ##### to see the route list
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Main page";
+});
+
+Route::get('/xxx', function () {
+    return 'Hello';
+})-> name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name){
+    return 'Hello '. $name . '!';
+});
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
 });
